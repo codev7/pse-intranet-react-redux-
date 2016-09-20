fea9ff6cd088fad8d6fe10d9ab4967e658e2bf85
Anthony Andriano <anthonyandriano@gmail.com>
2016-09-20 07:47:17 -0600

    added env variable for api server name

--- server/api/constants.js
+++ server/api/constants.js
@@ -1,10 +1,10 @@
 const CONFIG = {
+  API_SERVER_NAME: process.env.API_SERVER_NAME || 'https://api.pseglobal.com/',
   CLIENT_ID: process.env.CLIENT_ID || '123',
   CLIENT_SECRET: process.env.CLIENT_SECRET || '456',
   AUTH_GET_ACCESS_TOKEN: 'oauth/access_token',
   AUTH_GET_REFRESH_TOKEN: 'oauth/refresh_token',
   REVOKE_TOKEN: 'oauth/revoke_token',
-  API_SERVER_NAME: 'https://api.pseglobal.com/',
   GET_CURRENT_USER: 'me'
 }
 
